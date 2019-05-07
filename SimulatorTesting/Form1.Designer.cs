a6b7d0e1ebfd15491dd1e1339a732bcd2d3c5e3e
Emil Gravesen <gravesenemil@hotmail.com>
2019-05-07 21:46:31 +0200

    Add project files.

---
+++ SimulatorTesting/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SimulatorTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ObstacleButton = new System.Windows.Forms.Button();
+            this.GridCleanButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.FlipButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 118);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Testing";
+            // 
+            // ObstacleButton
+            // 
+            this.ObstacleButton.Location = new System.Drawing.Point(164, 4);
+            this.ObstacleButton.Name = "ObstacleButton";
+            this.ObstacleButton.Size = new System.Drawing.Size(75, 23);
+            this.ObstacleButton.TabIndex = 2;
+            this.ObstacleButton.Text = "Obstacles";
+            this.ObstacleButton.UseVisualStyleBackColor = true;
+            this.ObstacleButton.Click += new System.EventHandler(this.ObstacleButton_Click);
+            // 
+            // GridCleanButton
+            // 
+            this.GridCleanButton.Location = new System.Drawing.Point(245, 4);
+            this.GridCleanButton.Name = "GridCleanButton";
+            this.GridCleanButton.Size = new System.Drawing.Size(75, 23);
+            this.GridCleanButton.TabIndex = 3;
+            this.GridCleanButton.Text = "Grid cleaning";
+            this.GridCleanButton.UseVisualStyleBackColor = true;
+            this.GridCleanButton.Click += new System.EventHandler(this.GridCleanButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(326, 4);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 4;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // FlipButton
+            // 
+            this.FlipButton.Location = new System.Drawing.Point(407, 4);
+            this.FlipButton.Name = "FlipButton";
+            this.FlipButton.Size = new System.Drawing.Size(75, 23);
+            this.FlipButton.TabIndex = 5;
+            this.FlipButton.Text = "Flip";
+            this.FlipButton.UseVisualStyleBackColor = true;
+            this.FlipButton.Click += new System.EventHandler(this.FlipButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 664);
+            this.Controls.Add(this.FlipButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.GridCleanButton);
+            this.Controls.Add(this.ObstacleButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ObstacleButton;
+        private System.Windows.Forms.Button GridCleanButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button FlipButton;
+    }
+}
+
---
+++ SimulatorTesting/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SimulatorTesting
+{
+   
+    public partial class Form1 : Form
+    {
+        private Image Roomba = Image.FromFile("roomba.png");
+        private List<Rectangle> rectangles = new List<Rectangle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            pictureBox1.Height = Roomba.Height;
+            pictureBox1.Width = Roomba.Width;
+            pictureBox1.Image = Roomba;
+            
+                                    
+                                    
+        }
+
+        //Need obstacle management
+        private void GridCleaning()
+        {
+            while(pictureBox1.Location.Y < this.Height - 146 && pictureBox1.Location.X < this.Width - 119 - pictureBox1.Width)
+            {
+                while (pictureBox1.Location.Y < this.Height - 146)
+                {
+                    MoveDown(25);
+                    Thread.Sleep(100);
+                }
+                
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipY);
+                pictureBox1.Refresh();
+                MoveRight(pictureBox1.Width);
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipY);
+                pictureBox1.Refresh();
+                while (pictureBox1.Location.Y > 25)
+                {
+                    MoveUp(25);
+                    Thread.Sleep(100);
+                }
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipY);
+                pictureBox1.Refresh();
+                MoveRight(pictureBox1.Width);
+                pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipY);
+                pictureBox1.Refresh();
+
+            }
+            
+            MessageBox.Show("Done!");
+        }        
+        
+        private void CreateObstacles(int amount)
+        {
+            amount = amount + rectangles.Count;
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            while (rectangles.Count != amount)
+            {
+                int X = RandomClass.GetRandom(0, this.Width - 119);
+                int Y = RandomClass.GetRandom(0, this.Height - 146);
+                Rectangle rectangle = new Rectangle
+                {
+                    Height = 25,
+                    Width = 25,
+                    Location = new Point(X, Y)
+                };
+
+                if (!(pictureBox1.Bounds.IntersectsWith(rectangle)))
+                {
+                    rectangles.Add(rectangle);
+                    //rectangles.Add(rectangle);
+                    formGraphics.DrawRectangle(myPen, rectangle);
+                    formGraphics.FillRectangle(myBrush, rectangle);
+                }               
+
+            }
+            myBrush.Dispose();
+            formGraphics.Dispose();
+
+            
+
+        }
+        //All fucked yo with obsta
+        private void Form1_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            label1.Text = e.KeyChar.ToString();
+        }
+
+        private bool CollisionDetector()
+        {
+            bool NoCollisionDetected = false;
+            foreach (var rectangle in rectangles)
+            {
+                if (pictureBox1.Bounds.IntersectsWith(rectangle))
+                {
+                    NoCollisionDetected = true;
+                    return NoCollisionDetected;
+                }
+            }
+            return NoCollisionDetected;
+        }
+
+        private void MoveDown(int pixels)
+        {           
+            
+            if (pictureBox1.Location.Y < this.Height - 146 && !CollisionDetector())
+            {
+                label1.Text = String.Format("X: {0} Y: {1}", pictureBox1.Location.X.ToString(), pictureBox1.Location.Y.ToString());
+                pictureBox1.Top = pictureBox1.Top + pixels;
+            }
+            else
+            {
+                //pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 2);
+                //CollisionDetected = true;
+                int moveCount = 0;
+                while (CollisionDetector())
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                    moveCount++;
+                }
+                MoveDown(pictureBox1.Height + 25);
+                MoveLeft(moveCount);
+               
+
+
+            }
+
+        }
+
+        private void MoveUp(int pixels)
+        {
+            
+            if (pictureBox1.Location.Y > 10 && !CollisionDetector())
+            {
+                label1.Text = String.Format("X: {0} Y: {1}", pictureBox1.Location.X.ToString(), pictureBox1.Location.Y.ToString());
+                pictureBox1.Top = pictureBox1.Top - pixels;
+            }
+            else
+            {
+                int moveCount = 0;
+                while (CollisionDetector())
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                    moveCount++;
+                }
+                MoveUp(pictureBox1.Height + 25);
+                MoveLeft(moveCount);
+            }
+
+        }
+        private void MoveRight(int pixels)
+        {
+            
+            if (pictureBox1.Location.X < this.Width - 119 && !CollisionDetector())
+            {
+                label1.Text = String.Format("X: {0} Y: {1}", pictureBox1.Location.X.ToString(), pictureBox1.Location.Y.ToString());
+                pictureBox1.Left = pictureBox1.Left + pixels;
+            }
+            else
+            {
+                int moveCount = 0;
+                while (CollisionDetector())
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 1);
+                    moveCount++;
+                }
+                MoveRight(pictureBox1.Height + 25);
+                MoveUp(moveCount);
+            }
+
+        }
+        private void MoveLeft(int pixels)
+        {
+             
+            if (pictureBox1.Location.X > 10 && !CollisionDetector())
+            {
+                label1.Text = String.Format("X: {0} Y: {1}", pictureBox1.Location.X.ToString(), pictureBox1.Location.Y.ToString());
+                pictureBox1.Left = pictureBox1.Left - pixels;
+            }
+            else
+            {
+                int moveCount = 0;
+                while (CollisionDetector())
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 1);
+                    moveCount++;
+                }
+                MoveLeft(pictureBox1.Height + 25);
+                MoveUp(moveCount);
+            }
+        }
+        private void KeepMovingDown()
+        {
+            int count = 0;
+            while (true)
+            {
+                if (count > 10) break;
+                
+                MoveDown(5);
+                count++;
+            }
+        }
+        private void KeepMovingUp()
+        {
+            int count = 0;
+            while (true)
+            {
+                if (count > 10) break;
+
+                MoveUp(5);
+                count++;
+            }
+        }
+        private void KeepMovingRight()
+        {
+            int count = 0;
+            while (true)
+            {
+                if (count > 10) break;
+
+                MoveRight(5);
+                count++;
+            }
+        }
+        private void KeepMovingLeft()
+        {
+            int count = 0;
+            while (true)
+            {
+                if (count > 10) break;
+
+                MoveLeft(5);
+                count++;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //CreateObstacles(10);
+        }       
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {          
+
+            var Char = e.KeyChar.ToString();
+
+            switch(Char)
+            {
+                case "w":
+                    MoveUp(5);
+                    break;
+                case "s":
+                    MoveDown(5);
+                    break;
+                case "d":
+                    MoveRight(5);
+                    break;
+                case "a":
+                    MoveLeft(5);
+                    break;
+                case "t":
+                    KeepMovingUp();
+                    break;
+                case "g":
+                    KeepMovingDown();
+                    break;
+                case "h":
+                    KeepMovingRight();
+                    break;
+                case "f":
+                    KeepMovingLeft();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private void ObstacleButton_Click(object sender, EventArgs e)
+        {
+            CreateObstacles(10);
+        }
+
+        private void GridCleanButton_Click(object sender, EventArgs e)
+        {
+            GridCleaning();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(12, 37);
+        }
+
+        private void FlipButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipY);
+            pictureBox1.Refresh();
+        }
+    }
+    public class RandomClass
+    {
+        static Random random = new Random();
+
+
+        public static int GetRandom(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+    }
+}
